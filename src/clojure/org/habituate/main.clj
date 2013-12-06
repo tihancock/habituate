44962c9fec936ff1c30a18c94e6a6fc68086890f
Tom Hancock <thomas.ian.hancock@gmail.com>
2013-12-06 21:59:59 +0000

    Woohoo! Two activities connected, progress of a kind...

--- src/clojure/org/habituate/main.clj
+++ src/clojure/org/habituate/main.clj
@@ -1,13 +1,34 @@
 (ns org.habituate.main
   (:use [neko.activity :only [defactivity set-content-view!]]
         [neko.threading :only [on-ui]]
-        [neko.ui :only [make-ui]]))
+        [neko.ui :only [make-ui]])
+  (:import [android.content Intent]
+           [android.app Activity]))
 
-(defactivity org.habituate.MainActivity
-  :def a
+(def add-habit-layout
+  [:linear-layout {}
+   [:edit-text {:hint "Habit"
+                :id ::habit}]])
+
+(defactivity org.habituate.AddHabit
   :on-create
   (fn [this bundle]
     (on-ui
-     (set-content-view! a
-      (make-ui [:linear-layout {}
-                [:text-view {:text "Hello from Clojure!"}]])))))
+     (set-content-view! addHabit (make-ui add-habit-layout)))))
+
+(defn add-habit
+  [^Activity context]
+  (let [intent (Intent. context (Class/forName "org.habituate.AddHabit"))]
+    (.startActivity context intent)))
+
+(defn main-layout
+  [context]
+  [:linear-layout {}
+   [:button {:text "Add Habit"
+             :on-click (fn [_] (add-habit context))}]])
+
+(defactivity org.habituate.MainActivity
+  :on-create
+  (fn [this bundle]
+    (on-ui
+     (set-content-view! mainActivity (make-ui (main-layout this))))))
